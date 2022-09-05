07c9e8b8cc75f275665e9cd3d64ab01e5bbf2f41
Olga <politykoolga@outlook.com>
2022-09-05 15:15:25 +0300

    Three commit

---
+++ Example3/Program.cs
@@ -0,0 +1,35 @@
+﻿int a;                        
+Console.Write("Введите число, соответствующее дню недели: ");
+a = Convert.ToInt32(Console.ReadLine()); 
+
+switch (a)
+{
+    case 1:
+    Console.WriteLine("нет");
+    break;
+    
+    case 2:
+    Console.WriteLine("нет");
+    break;    
+
+    case 3:
+    Console.WriteLine("нет");
+    break;    
+
+    case 4:
+    Console.WriteLine("нет");
+    break;
+
+    case 5:
+    Console.WriteLine("нет");
+    break;
+
+    case 6:
+    Console.WriteLine("да");
+    break;
+
+    case 7:
+    Console.WriteLine("да");
+    break;
+
+    }
