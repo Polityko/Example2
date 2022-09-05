546c158a4a304d4b0fbd12e8ad68ff43141d560c
Olga <politykoolga@outlook.com>
2022-09-05 15:11:53 +0300

    Ferst commit

---
+++ Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿int rand = new Random().Next(1,65536);
+Console.WriteLine(rand);
+
+if (rand < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if (rand > 100);
+    int digit_3 = (rand / 100) % 10;
+    Console.WriteLine(digit_3);
+}
