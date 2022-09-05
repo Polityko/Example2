9e1530a198b9b6f18846742303586520ff894f37
Olga <politykoolga@outlook.com>
2022-09-05 15:18:27 +0300

    Four commit

---
+++ Example4/Program.cs
@@ -0,0 +1,14 @@
+﻿int rand = new Random().Next(1,1000000);
+Console.WriteLine(rand);
+
+
+
+if (rand % 7 == 0 && rand % 23 == 0)
+
+{ 
+    Console.WriteLine ("Кратное число");
+}
+else
+{
+    Console.WriteLine ("Некратное число");
+}
